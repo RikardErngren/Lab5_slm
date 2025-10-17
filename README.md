f9151d824331d685f1a0b02a0f20dfa6517a7a05
RikardErngren <rikard.erngren@hotmail.com>
2025-10-17 13:00:27 +0200

    Added a method to find transition frequencies as well as methods to print private variables. Also removed a check in find_k_gram_freqs that looked at whether or not a key was already in the map, but this is apparently not necessary as C++ default constructs in this instance.

--- README.md
+++ README.md
@@ -10,5 +10,8 @@ transition_freq - the frequencies of which characters come after the different k
 
 Note that output text length is not relevant to the language model, that's handled by the TextGenerator (To be continued).
 
-
 Saving the entire file to a string might not be ideal, but I'm assuming we're not working with huge files here. It's possible to just use a queue directly on the opened file, but then I would have to open the file twice since I want the program to be modular enough that I don't have to find both kgram_freqs and transition_freqs in the same method. But then again, I have to loop through the string twice anyway now.
+
+The reason I'm using deque instead of a normal queue is because a queue is not iterable and well... I need to iterate over it to set the k-gram consisting of the elements currently in the queue.
+
+Normalizing the frequencies immediately is probably not the greatest idea since small floating point errors will stack up, but I tested it for the whole text of A Modest Proposal and the numbers all added up to 0.999 so I'm OK with it and don't really feel like I have to make another method just to normalize the frequencies since them adding up to something closer to 1 doesn't really matter in the context of this project. For other projects I could definitely see this actually mattering. Here it doesn't matter if the percentages are ever so slightly off, if you're lucky it could even make real words appear more often, but it almost certainly won't make them appear less.
--- src/language_model.cpp
+++ src/language_model.cpp
@@ -5,44 +5,51 @@
 #include <deque>
 
 
+
 class LanguageModel {
-private: //should probably add getters for all these private variables
+private:
     int k;
     std::string filename; // not strictly necessary but it makes sense to have a language model know the name of the "language" (filename) it's trained on.
     std::map<std::string, float> k_gram_freqs;
-    std::map<std::string, std::map<char, int>> transition_freqs;
+    std::map<std::string, std::map<char, float>> transition_freqs;
 
 public:
     LanguageModel(int k, std::string filename) : k(k), filename(filename) {}
 
-
-    /* fairly hard to read code I admit... could perhaps be divided into smaller methods.
-    the idea is to use a queue to "slide" over the contents of the file  and always just 
-    remove the first letter of the k-gram so that you, in the next iteration when you ad
+    /* The following is fairly hard to read code I admit... could perhaps be divided into smaller methods.
+    The idea is to use a queue to "slide" over the contents of the file and always just 
+    remove the first letter of the k-gram so that you, in the next iteration when you add
     the next character, will have another k-gram that you add to the map. */ 
-    void find_k_gram_frequencies(std::string language) {
+    void find_k_gram_freqs(std::string language) {
         int k_gram_count = language.length() - k + 1;
         std::deque<char> q;
-        for (char c : language) {
+        // using '&' avoids using copies (which is more expensive) and instead just iterates by reference
+        for (char& c : language) {
             q.push_back(c);
             if (q.size() == k) {
                 std::string k_gram(q.begin(), q.end());
 
-                // count returns 1 or 0 so this just checks 
-                // if there's already a key with that name in the map 
-                if (k_gram_freqs.count(k_gram)) {
-                    k_gram_freqs[k_gram] += 1.0f / k_gram_count; //normalizes immediately
-                } else {
-                    k_gram_freqs.insert({k_gram, 1.0f / k_gram_count});
-                }
-                q.pop_front(); // removes first element of the queue
-            }
+                // don't have to check whether the key exists because C++ apparently default-constructs it automatically if it doesn't
+                k_gram_freqs[k_gram] += 1.0f / k_gram_count; // normalizes immediately
 
+                q.pop_front();
+            }
         }
     }
     
-    void get_transition_freqs() {}
-
+    /* There is some redundancy here. We use the same basic idea as in find_k_gram_freqs, where
+    we slide over the text and check what comes next for each occurence of a key. But I learned 
+    about the existence of substr which seems like a far superior way of doing this.*/
+    void find_transition_freqs(std::string language) {
+        int n = language.length();
+        int k_gram_count = language.length() - k + 1;
+        
+        for (int i=0; i < n-k; i++) {
+            std::string k_gram = language.substr(i, k); // substr(index, length)
+            char next_char = language[i+1];
+            transition_freqs[k_gram][next_char] += 1.0f / k_gram_count;
+        }
+    }
 
     void print_kgram_freqs() {
         for (const auto& [kgram, freq] : k_gram_freqs) {
@@ -50,11 +57,27 @@ public:
         }
     }
 
+    void print_transition_freqs() {
+        for (const auto& [kgram, char_and_freq] : transition_freqs) {
+            for (const auto& [next_char, freq] : char_and_freq) {
+            std::cout << kgram << " : " << next_char << " : " << freq << std::endl;
+            }
+        }
+    }
+
+    void print_filename() {
+        std::cout << filename << std::endl;
+    }
+
+    void print_k() {
+        std::cout << k << std::endl;
+    }
 };
 
+
 int main() {
-    int k = 3;
-    std::string filename = "test_text.txt";
+    int k = 6;
+    std::string filename = "a_modest_proposal.txt";
     LanguageModel slm(k, filename);
     std::ifstream datafile(filename);
 
@@ -64,11 +87,13 @@ int main() {
     std::string language((std::istreambuf_iterator<char>(datafile)),
                  std::istreambuf_iterator<char>()); 
 
-    if (language.length() < k) {
-        std::cerr << "the size of k needs to be less than the size of the whole text in the provided file." << std::endl;
+    if (language.length() <= k) {
+        std::cerr << "the size of k needs to be less than or equal to the size of the whole text in the provided file." << std::endl;
         return 1;
     }
 
-    slm.find_k_gram_frequencies(language);
-    slm.print_kgram_freqs();
+    slm.find_k_gram_freqs(language);
+    //slm.print_kgram_freqs();
+    slm.find_transition_freqs(language);
+    slm.print_transition_freqs();
 }
