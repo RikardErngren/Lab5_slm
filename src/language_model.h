a9cac51c1a8346a008f8b127831d036c0234bb0c
RikardErngren <rikard.erngren@hotmail.com>
2025-10-17 13:28:23 +0200

    Fixed the language_model.h file. This is not needed yet since I'm not using main.cpp, but it will be necessary later.

---
+++ src/language_model.h
@@ -0,0 +1,26 @@
+#include <map>
+#include <string>
+
+class LanguageModel {
+private:
+    int k;
+    std::string filename;
+    std::map<std::string, float> k_gram_freqs;
+    std::map<std::string, std::map<char, float>> transition_freqs;
+
+public:
+    LanguageModel(int k, std::string filename) : k(k), filename(filename) {}
+
+    void find_k_gram_freqs(std::string language) {}
+ 
+    void find_transition_freqs(std::string language) {}
+
+    void print_kgram_freqs() {}
+
+    void print_transition_freqs() {}
+
+    void print_filename() {}
+
+    void print_k() {}
+
+};
